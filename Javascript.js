2a372bd23807620c2fbe2fbac38e5556b228f0c7
MarkPerez <markperez88@hotmail.co.uk>
2016-06-17 16:12:32 +0100

    Exercise 3

--- Javascript.js
+++ Javascript.js
@@ -3,8 +3,8 @@ event1.addEventListener('click', activate, false);
 
 function activate(){
     var text = document.getElementById("input1").value;
-    document.getElementById("demo").innerHTML = text.slice(7,8);
-    document.getElementById("demo1").innerHTML = text.slice(7,11);
+    document.getElementById("demo").innerHTML = text.indexOf("string");
+   
     
    
 }
